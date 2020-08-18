81216ebdcd47cf7180e90f11a110032ae9ba3ae8
Will <william.w_@hotmail.com>
2020-08-18 17:45:58 +1000

    100% complete

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class PrintAndHide : MonoBehaviour
 {   
     int i = 0;
+    int blueNum;
     public Renderer rend;
     // Start is called before the first frame update
     void Start()
     {
-        
+        blueNum = Random.Range(200,250);
     }
 
     // Update is called once per frame
@@ -17,5 +18,14 @@ public class PrintAndHide : MonoBehaviour
     {
         Debug.Log(gameObject.name + i);
         i++;
+
+        if(i == 100)
+        {
+            GameObject.FindWithTag("Red").SetActive(false);
+        } else if (i == blueNum)
+        {
+            GameObject.FindWithTag("Blue").GetComponent<Renderer>().enabled = false;
+        }
+
     }
 }
